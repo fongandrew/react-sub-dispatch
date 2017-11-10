4b61b79002471dc63c5ce51988e77058ee1e6d5e
fongandrew <lx2798@gmail.com>
2017-11-09 19:56:54 -0800

    Boilerplate

---
+++ README.md
@@ -0,0 +1,49 @@
+# react-sub-dispatch
+[![Build Status](https://travis-ci.org/fongandrew/react-sub-dispatch.svg?branch=master)](https://travis-ci.org/fongandrew/react-sub-dispatch)
+
+A datastore-agnostic library for subscribing React components to a data source
+and dispatching events. Inspired by `react-redux`, but comes with a more
+generic interface for connecting stores.
+
+```ts
+connect(
+  (context, ownProps) => ({
+    prop1: [Sub1, { store: context.store, key1: props.key1 }],
+    prop2: [Sub2, { store: context.store, key2: props.key2 }]
+  }),
+
+  (context, dispatch, ownProps) => ({
+
+  }),
+
+  (context, subProps, dispatchProps, ownProps) => ({
+
+  })
+);
+
+connect(
+  (ownProps, context) => ({
+    prop1: [Sub1, { key1: props.key1 }, context],
+    prop2: [Sub2, { key2: props.key2 }, context]
+  }),
+
+  (dispatch, ownProps, context) => ({
+
+  }),
+
+  (subProps, dispatchProps, ownProps, context) => ({
+    ...subProps,
+    ...dispatchProps
+  })
+)
+
+```
+
+Could just use React context above.
+Or could use alt context set by provider ...
+
+```tsx
+<Provider dispatcher={} getContext={}>
+  <MyApp />
+</Provider>
+```
---
+++ src/types.ts
@@ -0,0 +1,60 @@
+/*
+  Common, shared types
+*/
+
+// Define Flux actions as a function of an ActionsMap -- a record mapping
+// actions types to other properties of that action
+export type Action<
+  ActionsMap,
+  Type extends keyof ActionsMap
+> = ActionsMap[Type] & {
+  type: Type;
+};
+
+// Handler processes a single action type with some services
+export interface Handler<Action, Services, Ret> {
+  (action: Action, services: Services): Ret;
+}
+
+// Generic version of the above, may handle multiple types, defined as
+// function of ActionsMap.
+export interface GenericHandler<
+  ActionsMap,
+  Services,
+  Ret
+> {
+  <Type extends keyof ActionsMap>(
+    action: Action<ActionsMap, Type>,
+    services: Services
+  ): Ret;
+}
+
+// Interface for list of active handlers
+export type HandlerListMap<ActionsMap, Services, Ret> = {
+  [Type in keyof ActionsMap]?: Handler<
+    Action<ActionsMap, Type>,
+    Services,
+    Ret
+  >[];
+};
+
+// Interface for adding handlers to Dispatcher
+export type HandlerAdditionsMap<ActionsMap, Services, Ret> = {
+  [Type in keyof ActionsMap]?: (
+    Handler<Action<ActionsMap, Type>, Services, Ret>[]|
+    Handler<Action<ActionsMap, Type>, Services, Ret>
+  );
+};
+
+// Provides services to action dispatch
+export interface Dispatcher<ActionsMap, Services, Ret> {
+
+  // Call handlers on action
+  dispatch<Type extends keyof ActionsMap>(
+    action: Action<ActionsMap, Type>
+  ): Ret;
+
+  // Services are public. Modifying the available services isn't necessarily
+  // an advisable thing, but we make it public for ease of mocking, etc.
+  services: Services;
+}
